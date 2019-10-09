34b20b999b009761b5d4e1d82170695171574092
Ben Vinegar <ben@sentry.io>
2019-10-08 20:15:33 -0700

    Server stack trace output in tests

--- index.test.js
+++ index.test.js
@@ -4,6 +4,15 @@ describe('index.js', () => {
   let server;
   beforeAll(() => {
     server = require('./index');
+    const {app, init} = require('./app')
+
+    app.use(function (err, req, res, next) {
+      console.error(err.stack); // Explicitly output any stack trace dumps to stderr
+      next(err, req, res);
+    });
+    const listener = app.listen(process.env.PORT, async function() {
+      init();
+    });
   });
 
   afterAll(() => {
