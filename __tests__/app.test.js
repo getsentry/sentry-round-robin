8d535c44e72b5c65b486af7a74b08a9deb5b3b91
Ben Vinegar <ben@sentry.io>
2019-10-14 21:12:11 -0700

    Add tests for no-op requests to webhook endpoint ...
    ... also, clear all nock interceptors between tests (avoids interceptors bleeding state between tests).

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -19,14 +19,6 @@ describe("app.js", () => {
   };
 
   beforeAll(async () => {
-    // Mock request for fetching users; persist between tests
-    nock(sentryAPIbase)
-      .persist() // Don't remove this interceptor when request received
-      .get(
-        `/organizations/${mockData.orgSlug}/users/?project=${mockData.projectID}`
-      )
-      .reply(200, mockData.getUsersResponse);
-
     app.use(function(err, req, res, next) {
       console.error(err.stack); // Explicitly output any stack trace dumps to stderr
       next(err, req, res);
@@ -35,10 +27,21 @@ describe("app.js", () => {
     server = await app.listen(process.env.PORT);
   });
 
-  // Reset state of user queue
-  afterEach(() => {
+  beforeEach(() => {
     app.allUsers = [...mockData.userNames];
     app.queuedUsers = [...mockData.userNames];
+
+    // Mock request for fetching users; persist between tests
+    nock(sentryAPIbase)
+      .get(
+        `/organizations/${mockData.orgSlug}/users/?project=${mockData.projectID}`
+      )
+      .reply(200, mockData.getUsersResponse);
+  });
+
+  // Reset state of user queue
+  afterEach(() => {
+    nock.cleanAll();
   });
 
   afterAll(() => {
@@ -47,13 +50,14 @@ describe("app.js", () => {
 
   test("Upon receiving POST request from Sentry with new issue data, server sends reponse 200", async function() {
     // Assign issue to mock user 1
-    nock(sentryAPIbase)
+    const request = nock(sentryAPIbase)
       .put(`/issues/${mockData.issueID}/`, {
         assignedTo: mockData.userNames[0]
       })
       .reply(200, mockData.assignIssueResponse(mockData.userNames[0]));
 
     let result = await sendRequest(newIssueRequestOptions);
+    expect(request.isDone()).toBe(true);
     expect(result).toBe("ok");
   });
 
@@ -63,13 +67,14 @@ describe("app.js", () => {
     expect(app.queuedUsers[0]).toBe(mockData.userNames[0]);
 
     // Assign issue to mock user 1
-    nock(sentryAPIbase)
+    const request = nock(sentryAPIbase)
       .put(`/issues/${mockData.issueID}/`, {
         assignedTo: mockData.userNames[0]
       })
       .reply(200, mockData.assignIssueResponse(mockData.userNames[0]));
 
     await sendRequest(newIssueRequestOptions);
+    expect(request.isDone()).toBe(true);
 
     // Expect user #1 to be removed, so user at index 0 is now user #2
     expect(app.queuedUsers.length).toBe(1);
@@ -160,5 +165,51 @@ describe("app.js", () => {
 
     // expect(request2.isDone()).toBe(true);
     expect(app.queuedUsers.length).toBe(1);
+  });
+
+  describe("no-ops", function () {
+    let request;
+    beforeEach(function () {
+      // Assign issue to mock user 1
+      request = nock(sentryAPIbase)
+        .put(`/issues/${mockData.issueID}/`)
+        .query(true)
+        .replyWithError('should never be called')
+
+    });
+
+    afterEach(() => {
+      nock.restore();
+    });
+
+    it("Should ignore webhook requests for non-issue resources", async function () {
+      let result = await sendRequest({
+        url: `http://127.0.0.1:${process.env.PORT}`,
+        method: "POST",
+        json: true,
+        headers: {
+          Authorization: "Bearer " + process.env.SENTRY_TOKEN,
+          "Sentry-Hook-Resource": "event"
+        },
+        body:{}
+      });
+      expect(result).toBe("ok");
+      expect(request.isDone()).toBe(false); // API never called
+    });
+
+    it("Should ignore webhook requests for issue resources that are not 'created' actions", async function () {
+      let result = await sendRequest({
+        url: `http://127.0.0.1:${process.env.PORT}`,
+        method: "POST",
+        json: true,
+        headers: {
+          Authorization: "Bearer " + process.env.SENTRY_TOKEN,
+          "Sentry-Hook-Resource": "issue"
+        },
+        body:{ action: "deleted" }
+      });
+      expect(result).toBe("ok");
+      expect(request.isDone()).toBe(false); // API never called
+    });
   });
 });
