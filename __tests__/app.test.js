b950f2a924e4da431e3961b9129cdc0e3eb7a7b4
Ben Vinegar <ben@sentry.io>
2019-10-14 20:19:51 -0700

    Tests now assert that mock requests to Sentry API were fired

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -20,29 +20,11 @@ describe("app.js", () => {
 
   beforeAll( async () => {
 
-    // Mock Sentry API responses
+    // Mock request for fetching users; persist between tests
     nock(sentryAPIbase)
       .persist() // Don't remove this interceptor when request received
       .get(`/organizations/${mockData.orgSlug}/users/?project=${mockData.projectID}`)
       .reply(200, mockData.getUsersResponse);
-
-    // Assign issue to mock user 1
-    nock(sentryAPIbase)
-      .persist()
-      .put(`/issues/${mockData.issueID}/`, {'assignedTo': mockData.userNames[0]})
-      .reply(200, mockData.assignIssueResponse(mockData.userNames[0]));
-
-    // Assign issue to mock user 2
-    nock(sentryAPIbase)
-      .persist()
-      .put(`/issues/${mockData.issueID}/`, {'assignedTo': mockData.userNames[1]})
-      .reply(200, mockData.assignIssueResponse(mockData.userNames[1]));
-
-    // Response for non-existent user
-    nock(sentryAPIbase)
-      .persist()
-      .put(`/issues/${mockData.issueID}/`, {'assignedTo': mockData.fakeUser})
-      .reply(400, mockData.getFakeUserResponse);
 
     app.use(function(err, req, res, next) {
       console.error(err.stack); // Explicitly output any stack trace dumps to stderr
@@ -65,6 +47,11 @@ describe("app.js", () => {
 
 
   test("Upon receiving POST request from Sentry with new issue data, server sends reponse 200", async function () {
+    // Assign issue to mock user 1
+    nock(sentryAPIbase)
+      .put(`/issues/${mockData.issueID}/`, {'assignedTo': mockData.userNames[0]})
+      .reply(200, mockData.assignIssueResponse(mockData.userNames[0]));
+
     let result = await sendRequest(newIssueRequestOptions);
     expect(result).toBe('ok');
   });
@@ -74,24 +61,45 @@ describe("app.js", () => {
     expect(app.queuedUsers.length).toBe(2);
     expect(app.queuedUsers[0]).toBe(mockData.userNames[0]);
 
+    // Assign issue to mock user 1
+    nock(sentryAPIbase)
+      .put(`/issues/${mockData.issueID}/`, {'assignedTo': mockData.userNames[0]})
+      .reply(200, mockData.assignIssueResponse(mockData.userNames[0]));
+
     await sendRequest(newIssueRequestOptions);
+
     // Expect user #1 to be removed, so user at index 0 is now user #2
     expect(app.queuedUsers.length).toBe(1);
     expect(app.queuedUsers[0]).toBe(mockData.userNames[1]);
   });
 
   test("When all users in queue are assigned an issue, queue is reset", async function () {
-    
+    const request1 = nock(sentryAPIbase)
+      .put(`/issues/${mockData.issueID}/`, {'assignedTo': mockData.userNames[0]})
+      .reply(200, mockData.assignIssueResponse(mockData.userNames[0]));
+
+    const request2 = nock(sentryAPIbase)
+      .put(`/issues/${mockData.issueID}/`, {'assignedTo': mockData.userNames[1]})
+      .reply(200, mockData.assignIssueResponse(mockData.userNames[1]));
+
     // Assign both mock users to issues, removing them form users queue
     await sendRequest(newIssueRequestOptions);
+    expect(request1.isDone()).toBe(true);
+
     await sendRequest(newIssueRequestOptions);
+    expect(request2.isDone()).toBe(true);
 
     // Expect queue to be empty
     expect(app.queuedUsers.length).toBe(0);
 
-    // Assign a third mock user, prompting queue to be reset
+    // Assign a third mock  user, prompting queue to be reset
+    const request3 = nock(sentryAPIbase)
+      .put(`/issues/${mockData.issueID}/`, {'assignedTo': mockData.userNames[0]})
+      .reply(200, mockData.assignIssueResponse(mockData.userNames[0]));
+
     await sendRequest(newIssueRequestOptions);
-    
+    expect(request3.isDone()).toBe(true);
+
     // User #1 immediately assigned and removed from queue,
     // so user at index 0 is now user #2
     expect(app.queuedUsers.length).toBe(1);
@@ -99,13 +107,24 @@ describe("app.js", () => {
   });
 
   test("When a user no longer exists, reassign to subsequent users until successful", async function () {
+    // Response for non-existent user
+    const request1 = nock(sentryAPIbase)
+      .put(`/issues/${mockData.issueID}/`, {'assignedTo': mockData.fakeUser})
+      .reply(400, mockData.getFakeUserResponse);
+
+    // Response for second (valid) user
+    const request2 = nock(sentryAPIbase)
+      .put(`/issues/${mockData.issueID}/`, {'assignedTo': mockData.userNames[1]})
+      .reply(200, mockData.assignIssueResponse(mockData.userNames[1]));
+
     // Override with mock user that doesn't exist in the mock API
     app.allUsers[0] = mockData.fakeUser;
     app.queuedUsers[0] = mockData.fakeUser;
 
-  
     // Assign both mock users to issues, removing them form users queue
     await sendRequest(newIssueRequestOptions);
+    expect(request1.isDone()).toBe(true); // 400
+    expect(request2.isDone()).toBe(true); // 200 (success)
 
     // Expect queue to be empty after removing nonexistent user #1
     // and then assigning/removing user #2
@@ -118,8 +137,17 @@ describe("app.js", () => {
     app.allUsers = [];
     app.queuedUsers = [];
 
+    // Response for second (valid) user
+    const request = nock(sentryAPIbase)
+      .put(`/issues/${mockData.issueID}/`, {'assignedTo': mockData.userNames[0]})
+      .reply(200, mockData.assignIssueResponse(mockData.userNames[0]));
+
     // Create mocked new issue, triggering update of user queue
     await sendRequest(newIssueRequestOptions);
+    expect(request.isDone()).toBe(true);
+
+    // expect(request2.isDone()).toBe(true);
+    expect(app.queuedUsers.length).toBe(1);
   });
 
 });
--- __tests__/mockdata.js
+++ __tests__/mockdata.js
@@ -1,65 +1,134 @@
 // Test env vars to point to fake org and project to match mocked API
-const sentryDSN = 'https://test@sentry.io/12345';
-const sentryToken = 'abc123';
-const orgSlug = 'testOrg';
-const projectID = '123456';
+const sentryDSN = "https://test@sentry.io/12345";
+const sentryToken = "abc123";
+const orgSlug = "testOrg";
+const projectID = "123456";
 
 // Sample Sentry data for API requests
-const issueID = '987654';
-const userNames = ['testEmail@test.com', 'otherTestEmail@test.com'];
-const newIssueRequestBody = { action: "created", data: { issue: { id: issueID} } };
+const issueID = "987654";
+const userNames = ["testEmail@test.com", "otherTestEmail@test.com"];
+const newIssueRequestBody = {
+  action: "created",
+  data: { issue: { id: issueID } }
+};
 
 // Sentry API sample response for unknown user or user without permissions:
-const getFakeUserResponse = {"assignedTo":["Unknown actor input"]}
+const getFakeUserResponse = { assignedTo: ["Unknown actor input"] };
 // NOTE: Can also be this message:
 // {"assignedTo":["Cannot assign to non-team member"]}
 // Status code for both of the above:
 const noUserStatusCode = 400;
-const fakeUser = 'nobody@test.com';
+const fakeUser = "nobody@test.com";
 
 // Sentry API sample response for .get(`/organizations/${orgSlug}/users/?project=${projectID}`)
-const getUsersResponse = [{"dateCreated":"2019-05-09T18:06:01.728Z","user":{"username":userNames[0],"lastLogin":"2019-09-16T02:56:06.806Z","isSuperuser":false,"isManaged":false,"lastActive":"2019-10-08T15:05:38.715Z","isStaff":false,"id":"433307","isActive":true,"has2fa":false,"name":"OtherTest McTestuser","avatarUrl":"https://secure.gravatar.com/avatar/1eb103c0e899f372a85eb0a44f0a0f42?s=32&d=mm","dateJoined":"2019-05-09T18:06:01.443Z","emails":[{"is_verified":true,"id":"468229","email":userNames[0]}],"avatar":{"avatarUuid":null,"avatarType":"letter_avatar"},"hasPasswordAuth":false,"email":userNames[0]},"roleName":"Organization Owner","expired":false,"id":"9376061","projects":["buggy-sentry-project"],"name":"OtherTest McTestuser","role":"owner","flags":{"sso:linked":false,"sso:invalid":false},"email":userNames[0],"pending":false},{"dateCreated":"2019-09-30T16:06:51.949Z","user":{"username":userNames[1],"lastLogin":"2019-09-30T16:08:10.517Z","isSuperuser":false,"isManaged":false,"lastActive":"2019-10-02T23:15:43.773Z","isStaff":false,"id":"518100","isActive":true,"has2fa":false,"name":"OtherTest McTestuser","avatarUrl":"https://secure.gravatar.com/avatar/7828bc81ef4bbd6d38d749803e1a02c6?s=32&d=mm","dateJoined":"2019-09-30T16:08:09.839Z","emails":[{"is_verified":true,"id":"562269","email":userNames[1]}],"avatar":{"avatarUuid":null,"avatarType":"letter_avatar"},"hasPasswordAuth":true,"email":userNames[1]},"roleName":"Member","expired":false,"id":"9496972","projects":["buggy-sentry-project"],"name":"OtherTest McTestuser","role":"member","flags":{"sso:linked":false,"sso:invalid":false},"email":userNames[1],"pending":false}];
+const getUsersResponse = [
+  {
+    dateCreated: "2019-05-09T18:06:01.728Z",
+    user: {
+      username: userNames[0],
+      lastLogin: "2019-09-16T02:56:06.806Z",
+      isSuperuser: false,
+      isManaged: false,
+      lastActive: "2019-10-08T15:05:38.715Z",
+      isStaff: false,
+      id: "433307",
+      isActive: true,
+      has2fa: false,
+      name: "OtherTest McTestuser",
+      avatarUrl:
+        "https://secure.gravatar.com/avatar/1eb103c0e899f372a85eb0a44f0a0f42?s=32&d=mm",
+      dateJoined: "2019-05-09T18:06:01.443Z",
+      emails: [{ is_verified: true, id: "468229", email: userNames[0] }],
+      avatar: { avatarUuid: null, avatarType: "letter_avatar" },
+      hasPasswordAuth: false,
+      email: userNames[0]
+    },
+    roleName: "Organization Owner",
+    expired: false,
+    id: "9376061",
+    projects: ["buggy-sentry-project"],
+    name: "OtherTest McTestuser",
+    role: "owner",
+    flags: { "sso:linked": false, "sso:invalid": false },
+    email: userNames[0],
+    pending: false
+  },
+  {
+    dateCreated: "2019-09-30T16:06:51.949Z",
+    user: {
+      username: userNames[1],
+      lastLogin: "2019-09-30T16:08:10.517Z",
+      isSuperuser: false,
+      isManaged: false,
+      lastActive: "2019-10-02T23:15:43.773Z",
+      isStaff: false,
+      id: "518100",
+      isActive: true,
+      has2fa: false,
+      name: "OtherTest McTestuser",
+      avatarUrl:
+        "https://secure.gravatar.com/avatar/7828bc81ef4bbd6d38d749803e1a02c6?s=32&d=mm",
+      dateJoined: "2019-09-30T16:08:09.839Z",
+      emails: [{ is_verified: true, id: "562269", email: userNames[1] }],
+      avatar: { avatarUuid: null, avatarType: "letter_avatar" },
+      hasPasswordAuth: true,
+      email: userNames[1]
+    },
+    roleName: "Member",
+    expired: false,
+    id: "9496972",
+    projects: ["buggy-sentry-project"],
+    name: "OtherTest McTestuser",
+    role: "member",
+    flags: { "sso:linked": false, "sso:invalid": false },
+    email: userNames[1],
+    pending: false
+  }
+];
 
 // Sentry API sample response for .put(`/issues/${issueID}/`, {'assignedTo': userNames[0]})
-function assignIssueResponse (userName) {
+function assignIssueResponse(userName) {
   return {
-  platform: 'javascript',
-  lastSeen: '2019-10-08T18:10:09.708Z',
-  numComments: 0,
-  userCount: 1,
-  culprit: '?(script)',
-  title: 'ReferenceError: gottaCatchEmAll is not defined',
-  id: issueID,
-  assignedTo: 
-   { type: 'user',
-     email: userName,
-     name: 'Test McTestuser',
-     id: '433307' },
-  logger: null,
-  type: 'error',
-  annotations: [],
-  metadata: 
-   { type: 'ReferenceError',
-     value: 'gottaCatchEmAll is not defined',
-     filename: '/script.js' },
-  status: 'unresolved',
-  subscriptionDetails: null,
-  isPublic: false,
-  hasSeen: false,
-  shortId: 'BUGGY-SENTRY-PROJECT-1W',
-  shareId: null,
-  firstSeen: '2019-10-08T18:10:09.708Z',
-  count: '1',
-  permalink: null,
-  level: 'error',
-  isSubscribed: false,
-  isBookmarked: false,
-  project: 
-   { platform: '',
-     slug: 'buggy-sentry-project',
-     id: projectID,
-     name: 'Quick Little Bug (JS, Glitch)' },
-  statusDetails: {}
+    platform: "javascript",
+    lastSeen: "2019-10-08T18:10:09.708Z",
+    numComments: 0,
+    userCount: 1,
+    culprit: "?(script)",
+    title: "ReferenceError: gottaCatchEmAll is not defined",
+    id: issueID,
+    assignedTo: {
+      type: "user",
+      email: userName,
+      name: "Test McTestuser",
+      id: "433307"
+    },
+    logger: null,
+    type: "error",
+    annotations: [],
+    metadata: {
+      type: "ReferenceError",
+      value: "gottaCatchEmAll is not defined",
+      filename: "/script.js"
+    },
+    status: "unresolved",
+    subscriptionDetails: null,
+    isPublic: false,
+    hasSeen: false,
+    shortId: "BUGGY-SENTRY-PROJECT-1W",
+    shareId: null,
+    firstSeen: "2019-10-08T18:10:09.708Z",
+    count: "1",
+    permalink: null,
+    level: "error",
+    isSubscribed: false,
+    isBookmarked: false,
+    project: {
+      platform: "",
+      slug: "buggy-sentry-project",
+      id: projectID,
+      name: "Quick Little Bug (JS, Glitch)"
+    },
+    statusDetails: {}
   };
 }
 
@@ -77,4 +146,3 @@ module.exports = {
   noUserStatusCode,
   fakeUser
 };
-
