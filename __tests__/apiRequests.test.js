534882d29ec1f2076229c418476c66bba7c7ec16
Ben Vinegar <ben@sentry.io>
2019-10-14 17:11:34 -0700

    Use async/await syntax in apiRequests.test.js ...
    To match the style of index.test.js

--- __tests__/apiRequests.test.js
+++ __tests__/apiRequests.test.js
@@ -12,58 +12,51 @@ afterEach(() => {
 
 describe('getProjectUsers ', () => {
 
-  test('succeeds with an array of expected usernames', () => {
+  test('succeeds with an array of expected usernames', async () => {
     nock(sentryAPIbase)
       .get(`/organizations/${mockData.orgSlug}/users/?project=${mockData.projectID}`)
       .reply(200, mockData.getUsersResponse);
 
-    return getProjectUsers(mockData.projectID, mockData.orgSlug, sentryAPIbase)
-      .then(res => {
-        expect(res).toContain(mockData.userNames[0]);
-        expect(res).toContain(mockData.userNames[1]);
-        expect(res).toHaveLength(2);
-      });
+    const res = await getProjectUsers(mockData.projectID, mockData.orgSlug, sentryAPIbase)
+
+    expect(res).toContain(mockData.userNames[0]);
+    expect(res).toContain(mockData.userNames[1]);
+    expect(res).toHaveLength(2);
   });
 
 
-  test('fails with empty array', () => {
+  test('fails with empty array', async () => {
     nock(sentryAPIbase)
       .get(`/organizations/${mockData.orgSlug}/users/?project=${mockData.projectID}`)
       .reply(404, {"detail":"The requested resource does not exist"});
 
-    return getProjectUsers(mockData.projectID, mockData.orgSlug, sentryAPIbase)
-      .then(res => {
-        expect(res).toHaveLength(0);
-      });
+    const res = await getProjectUsers(mockData.projectID, mockData.orgSlug, sentryAPIbase);
+    expect(res).toHaveLength(0);
   });
 
 });
 
 
 describe('assignIssue ', () => {
-  
-  test('succeeds with status 200 and ...', () => {
+
+  test('succeeds with status 200 and ...', async () => {
     nock(sentryAPIbase)
      .put(`/issues/${mockData.issueID}/`, {'assignedTo': mockData.userNames[0]})
      .reply(200, mockData.assignIssueResponse(mockData.userNames[0]));
 
-    return assignIssue(mockData.issueID, mockData.userNames[0]).then(res => {
-        expect(res.id).toBe(mockData.issueID);
-        expect(res.assignedTo.email).toBe(mockData.userNames[0]);
-      });
+    const res = await assignIssue(mockData.issueID, mockData.userNames[0])
+
+    expect(res.id).toBe(mockData.issueID);
+    expect(res.assignedTo.email).toBe(mockData.userNames[0]);
   });
 
-  test(' where user doesnt exist fails with status code for no user', () => {
+  test(' where user doesnt exist fails with status code for no user', async () => {
     nock(sentryAPIbase)
      .put(`/issues/${mockData.issueID}/`, {'assignedTo': mockData.userNames[0]})
       .reply(mockData.noUserStatusCode, {"assignedTo":["Unknown actor input"]});
 
-    return assignIssue(mockData.issueID, mockData.userNames[0])
-      .then(res => {
-        expect(res).toBe(mockData.noUserStatusCode);
-      });
+    const res = await assignIssue(mockData.issueID, mockData.userNames[0]);
+    expect(res).toBe(mockData.noUserStatusCode);
   });
-
-
 });
 
