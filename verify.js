4bbf2665a0f8cdb20ba783964e7bb89ac0e30471
Ben Vinegar <ben@sentry.io>
2019-10-15 22:34:38 -0700

    Access proper header (lowercase)

--- verify.js
+++ verify.js
@@ -3,8 +3,9 @@ const crypto = require('crypto');
 function verifySignature(request) {
   // See: https://docs.sentry.io/workflow/integrations/integration-platform/webhooks/#verifying-the-signature
   const hmac = crypto.createHmac("sha256", process.env.SENTRY_API_SECRET);
-  const expected = hmac.update(JSON.stringify(request.body), 'utf8');
-  return hmac.digest('hex') === request.headers["Sentry-Hook-Signature"];
+  hmac.update(JSON.stringify(request.body), 'utf8');
+  const digest = hmac.digest('hex');
+  return hmac.digest('hex') === request.headers["sentry-hook-signature"];
 }
 
 module.exports = verifySignature;
