87c92b665e19f69c75eb0036344f3b2e4dc679bf
Ben Vinegar <ben@sentry.io>
2019-10-15 22:55:30 -0700

    Fix tests, decouple verifySignature from process.env

--- __tests__/verify.test.js
+++ __tests__/verify.test.js
@@ -1,25 +1,21 @@
 const verifySignature = require("../verify");
 
 describe("verify.js", () => {
-  beforeAll(() => {
-    process.env.SENTRY_API_SECRET = "abc123";
-  });
-
   it("should verify a valid signature", () => {
     expect(verifySignature({
       body: { issue: 123456 },
       headers: {
-        "Sentry-Hook-Signature": "0e2041f59516f3d4507b22216f2e0050931692c1ae71c4207d38d79781963dfa"
+        "sentry-hook-signature": "0e2041f59516f3d4507b22216f2e0050931692c1ae71c4207d38d79781963dfa"
       }
-    })).toBe(true);
+    }, "abc123")).toBe(true);
   });
   
   it("should reject an invalid signature", () => {
     expect(verifySignature({
         body: { issue: 1234567890 },
         headers: {
-          "Sentry-Hook-Signature": "0e2041f59516f3d4507b22216f2e0050931692c1ae71c4207d38d79781963dfa"
+          "sentry-hook-signature": "0e2041f59516f3d4507b22216f2e0050931692c1ae71c4207d38d79781963dfa"
         }
-      })).toBe(false);
+      }, "abc123")).toBe(false);
   });
 });
--- verify.js
+++ verify.js
@@ -1,10 +1,11 @@
 const crypto = require('crypto');
 
-function verifySignature(request) {
+function verifySignature(request, secret='') {
   // See: https://docs.sentry.io/workflow/integrations/integration-platform/webhooks/#verifying-the-signature
-  const hmac = crypto.createHmac("sha256", process.env.SENTRY_API_SECRET);
+  const hmac = crypto.createHmac("sha256", secret);
   hmac.update(JSON.stringify(request.body), 'utf8');
   const digest = hmac.digest('hex');
+  console.log(digest);
   return digest === request.headers["sentry-hook-signature"];
 }
 
