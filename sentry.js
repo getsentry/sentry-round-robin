9cade66b103105d049b824e7f31000bccd6cfa74
Liz Krane <learningnerd@gmail.com>
2019-12-09 11:06:24 -0800

    Use if(x) instead of (x != undefined)
    for sentryDSN and sentryRelease

--- sentry.js
+++ sentry.js
@@ -2,13 +2,14 @@ const { sentryDSN, sentryRelease } = require("./constants").constants;
 const sentry = require("@sentry/node");
 
 // Initialize Sentry only if DSN is configured
-if (sentryDSN != null) {
+if (sentryDSN) {
 
   const sentryConfig = {
     dsn: sentryDSN
   };
 
-  if (sentryRelease != undefined) {
+  // Only set a release if defined in env vars (or .env)
+  if (sentryRelease) {
     sentryConfig.release = sentryRelease
   }
 
